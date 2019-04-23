6c1bede1c058f2c5feab722d7bef9c7b56ca5e81
Vladislav Kuznecov <super.ewew2015@yandex.ru>
2019-04-23 23:56:54 +0300

    Добавлено react, ts, tslint, prettier

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+const App = () => {
+  return (
+    <div className="container">
+      Hello Typescript
+    </div>
+  )
+}
+
+export { App };
